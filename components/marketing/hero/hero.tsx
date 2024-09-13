3530fbc9715b5350a3b11b20a8d210cb185c0f0e
russuiulia <russuiulia.md@gmail.com>
2024-09-13 15:05:39 +0300

    upd

--- components/marketing/hero/hero.tsx
+++ components/marketing/hero/hero.tsx
@@ -1,55 +1,50 @@
-import NextLink from "next/link";
-import {Button, Link, Chip, Snippet} from "@nextui-org/react";
-import {ArrowRightIcon} from "@nextui-org/shared-icons";
-import dynamic from "next/dynamic";
+// 'use client';
+import NextLink from 'next/link';
+import { Button } from '@nextui-org/react';
+import dynamic from 'next/dynamic';
 
-import {FloatingComponents} from "./floating-components";
+import { FloatingComponents } from './floating-components';
 
-import {GithubIcon} from "@/components/icons";
-import {title, subtitle} from "@/components/primitives";
-import {trackEvent} from "@/utils/va";
+import { title, subtitle } from '@/components/primitives';
 
-const BgLooper = dynamic(() => import("./bg-looper").then((mod) => mod.BgLooper), {
-  ssr: false,
-});
+const BgLooper = dynamic(
+  () => import('./bg-looper').then((mod) => mod.BgLooper),
+  {
+    ssr: false,
+  }
+);
 
-import {client} from "../../../sanity/client";
-import {useCurrentLocale} from "../../../locales/client";
-import {useEffect, useState} from "react";
+import { client } from '../../../sanity/client';
 
-// export async function getStaticProps() {
-//   const locale = useCurrentLocale();
-//   console.log(locale);
-//   const query = `*[_type == "hero" && language == "${locale}"]{
-//     title,subtitle,paragraph,buttonText
-//     }`;
+async function fetchHeroData(locale) {
+  const query = `*[_type == "hero" && language == "${locale}"]{
+      title, subtitle, paragraph, buttonText
+    }`;
+  const heroData = await client.fetch(query);
+  return (
+    heroData[0] || { title: '', subtitle: '', paragraph: '', buttonText: '' }
+  );
+}
 
-//   const heroData = await client.fetch(query);
-//   return heroData[0];
-// }
-
-export const Hero = ({heroData}) => {
-  // const [heroData, setHeroData] = useState({
-  //   title: "",
-  //   subtitle: "",
-  //   paragraph: "",
-  //   buttonText: "",
-  // });
-  // getStaticProps().then((data) => {
-  //   setHeroData(data);
-  // });
+export const Hero = async ({ locale }) => {
+  const heroData = await fetchHeroData(locale);
 
   return (
     <section className="flex relative overflow-hidden lg:overflow-visible w-full flex-nowrap justify-between items-center h-[calc(100vh_-_64px)] 2xl:h-[calc(84vh_-_64px)]">
       <div className="flex relative z-20 flex-col gap-6 w-full lg:w-1/2 xl:mt-10">
         <div className="text-center leading-8 md:leading-10 md:text-left">
           <div>
-            <h1 className={title({size: "lg"})}>{heroData.title}</h1>
+            <h1 className={title({ size: 'lg' })}>{heroData.title}</h1>
           </div>
           <div className="mt-6 mb-4">
-            <h1 className={title({color: "violet"})}>{heroData.subtitle}</h1>
+            <h1 className={title({ color: 'violet' })}>{heroData.subtitle}</h1>
           </div>
-          <h2 className={subtitle({fullWidth: true, class: "text-center md:text-left"})}>
+          <h2
+            className={subtitle({
+              fullWidth: true,
+              class: 'text-center md:text-left',
+            })}
+          >
             {heroData.paragraph}
           </h2>
         </div>
@@ -68,9 +63,11 @@ export const Hero = ({heroData}) => {
         </div>
       </div>
 
-    {/*   <FloatingComponents />*/}
+      {/*   <FloatingComponents />*/}
 
-      <BgLooper /> 
+      <BgLooper />
     </section>
   );
 };
+
+export const revalidate = 30;
